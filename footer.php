65d53c8cdf0321ef06ffa497be56730b6c2e68f1
Victor <victor.pomat@gmail.com>
2021-07-25 10:26:53 +0200

    Memory game v2

--- footer.php
+++
@@ -1,6 +0,0 @@
-    <!-- Ajout des script nécessaires au fonctionnement du projet -->
-    <script src="vendor/components/jquery/jquery.min.js"></script>
-    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="js/memory.js"></script>
-</body>
-</html>
